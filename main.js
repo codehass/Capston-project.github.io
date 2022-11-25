7d5f4571e370127f973808eca70842793f974128
iamhassan <hassan.elouardy06@gmail.com>
2022-11-25 21:19:34 +0100

    Add html and css

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ document.querySelector('.hamburger').addEventListener('click', () => {
   document.querySelector('.menu-ele').classList.add('show');
   document.querySelector('.list-header').classList.add('show');
   document.querySelector('.hamburger').classList.add('hidden');
+  document.querySelector('.close-menu').classList.add('show-menu');
 });
 
-// document.querySelector('.list-header').addEventListener('click', () => {
-//   document.querySelector('.hamburger').classList.remove('show');
-//   document.querySelector('.list-header').classList.remove('hidden');
-// });
+document.querySelector('.close-menu').addEventListener('click', () => {
+  document.querySelector('.menu-ele').classList.remove('show');
+  document.querySelector('.menu-ele').classList.add('hidden');
+  document.querySelector('.close-menu').classList.remove('show-menu');
+  document.querySelector('.hamburger').classList.remove('hidden');
+  document.querySelector('.list-header').classList.remove('show');
+});
